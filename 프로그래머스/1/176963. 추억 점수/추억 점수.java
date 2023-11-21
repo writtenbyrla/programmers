148dfe9644cdf41780beb6f9d92e2e1f95e41e3e
writtenbyrla <133620285+writtenbyrla@users.noreply.github.com>
2023-11-21 18:46:23 +0900

    [level 1] Title: 추억 점수, Time: 0.04 ms, Memory: 74.9 MB -BaekjoonHub

--- 프로그래머스/1/176963. 추억 점수/추억 점수.java
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.java
@@ -1,17 +1,10 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Arrays;
+import java.util.*;
 
 class Solution {
     public List<Integer> solution(String[] name, int[] yearning, String[][] photo) {
-int[] answer = {};
+        
         List<Integer> list = new ArrayList<>();
         
-        int grade = 0;
-
-        // 이름, 그리움 점수 map 만들기
         Map<String, Integer> map = new HashMap<String, Integer>();
         for(int i=0; i<name.length; i++){
           map.put(name[i], yearning[i]);
@@ -22,18 +15,12 @@ int[] answer = {};
         	for(String person: name) {
         		for(int j=0; j<photo[i].length; j++) {
         			if(person.equals(photo[i][j])) {
-        				grade = map.get(person);
-        				System.out.println("grade" + grade);
-        				sum += grade;
-        				System.out.println("sum" + sum);
+        				sum += map.get(person);
         			}
         		}
-
         	}
         	list.add(sum);	
-        }
-        
-        
+        }  
         return list;
     }
 }
