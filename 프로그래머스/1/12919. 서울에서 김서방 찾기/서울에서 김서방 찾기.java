e57bfa1cd5366757d1b5eabbbd607d46a50e5f79
writtenbyrla <133620285+writtenbyrla@users.noreply.github.com>
2023-12-08 22:06:17 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 6.73 ms, Memory: 78.9 MB -BaekjoonHub

--- 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
@@ -5,6 +5,7 @@ class Solution {
         for(int i=0; i<seoul.length; i++){
             if(seoul[i].equals("Kim")){
                 answer = "김서방은 " + i + "에 있다";
+                break;
             }
         }
         
