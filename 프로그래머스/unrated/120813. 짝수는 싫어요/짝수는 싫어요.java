80a9f9b3a24b8afadef779bd15a533253a6e7c8f
writtenbyrla <133620285+writtenbyrla@users.noreply.github.com>
2023-12-10 00:20:28 +0900

    [level unrated] Title: 짝수는 싫어요, Time: 0.02 ms, Memory: 74.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int[] solution(int n) {
+        int[] arr = new int[n];
+        for(int i=0; i<n; i++){
+            arr[i] = i+1;
+        }
+        
+        
+        int[] answer = {};
+        
+        if(n%2 == 0){
+            answer = new int[n/2];
+        } else {
+            answer = new int[n/2+1];
+        }
+       
+        for(int i=0; i<answer.length; i++){
+            answer[i]=arr[i*2];
+        }
+        
+        
+        
+
+        return answer;
+    }
+}
