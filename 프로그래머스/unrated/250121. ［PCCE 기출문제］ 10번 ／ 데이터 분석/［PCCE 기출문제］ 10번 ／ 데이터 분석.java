376bcc2ec22a4307e3b8b331725cf98f185a8b6b
writtenbyrla <133620285+writtenbyrla@users.noreply.github.com>
2023-12-12 11:40:21 +0900

    [level unrated] Title: [PCCE 기출문제] 10번 / 데이터 분석, Time: 2.31 ms, Memory: 75.4 MB -BaekjoonHub

--- 프로그래머스/unrated/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.java
+++ 프로그래머스/unrated/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.java
@@ -1,34 +1,28 @@
-import java.util.Arrays;
-import java.util.Comparator;
+import java.util.*;
+
 class Solution {
     public int[][] solution(int[][] data, String ext, int val_ext, String sort_by) {
-        int count = 0;
-        for(int i=0; i<data.length; i++){
-            if(ext.equals("code")&&(data[i][0]<val_ext)){
-                count++;
-            }else if(ext.equals("date")&&(data[i][1]<val_ext)){
-                count++;
-            }else if(ext.equals("maximum")&&(data[i][2]<val_ext)){
-                count++;
-            }else if(ext.equals("remain")&&(data[i][3]<val_ext)){
-                count++;
+        
+        String[] arr = {"code", "date", "maximum", "remain"};
+        int index = 0;
+        for(int i=0; i<arr.length; i++){
+            if(ext.equals(arr[i])){
+                index = i ;
             }
         }
         
-        int[][] answer = new int[count][4];
-        int answerIndex = 0;
+        List<int[]> list = new ArrayList<>();
         for(int i=0; i<data.length; i++){
-                if(ext.equals("code")&&(data[i][0]<val_ext)){
-                    answer[answerIndex++] = Arrays.copyOf(data[i], data[i].length);
-                }else if(ext.equals("date")&&(data[i][1]<val_ext)){
-                    answer[answerIndex++] = Arrays.copyOf(data[i], data[i].length);
-                }else if(ext.equals("maximum")&&(data[i][2]<val_ext)){
-                    answer[answerIndex++] = Arrays.copyOf(data[i], data[i].length);
-                }else if(ext.equals("remain")&&(data[i][3]<val_ext)){
-                    answer[answerIndex++] = Arrays.copyOf(data[i], data[i].length);
-                }
+            if(data[i][index]<val_ext){
+                list.add(data[i]);
+            }
+        }    
+            
+        int[][] answer = new int[list.size()][4];
+        for(int i=0; i<answer.length; i++){
+            answer[i] = list.get(i);
         }
-        
+
         if(sort_by.equals("code")){
             Arrays.sort(answer, Comparator.comparingInt(row -> row[0]));      
         }else if(sort_by.equals("date")){
@@ -38,7 +32,7 @@ class Solution {
         }else if(sort_by.equals("remain")){
             Arrays.sort(answer, Comparator.comparingInt(row -> row[3]));
         }
-        
+
         return answer;
     }
 }
