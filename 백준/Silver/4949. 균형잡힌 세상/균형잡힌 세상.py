b1d5a0b8ead2252eee53dafbaa41e0d9dd9f8a95
writtenbyrla <133620285+writtenbyrla@users.noreply.github.com>
2024-01-09 17:37:08 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 348 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.py
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.py
@@ -1,25 +1,22 @@
 pair = {')' : '(', ']' : '['}
 
-while(True):
+while True:
     sentence = input()
-
     if sentence == ".": break
-
     stack = []
     result = True
-
+    
     for char in sentence:
         if char in '([':
             stack.append(char)
-
+            
         elif char in ')]':
             if len(stack)!=0 and stack[-1] == pair[char]:
                 stack.pop()
             else:
-                stack.append(char)
                 result = False
                 break
-
+                
     if not stack and result:
         print('yes')
     else:
