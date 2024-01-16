ad0454ae59dba6d96ea036abd74e718c387c31c2
writtenbyrla <133620285+writtenbyrla@users.noreply.github.com>
2024-01-16 20:14:07 +0900

    [Gold V] Title: 적록색약, Time: 72 ms, Memory: 31692 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.py
+++ 백준/Gold/10026. 적록색약/적록색약.py
@@ -1,56 +1,53 @@
 import sys
-input = sys.stdin.readline
 sys.setrecursionlimit(10000)
+n = int(sys.stdin.readline())
 
-def dfs(x, y, color, isblind):
+array = [[]*n for _ in range(n)]
+for i in range(n):
+    array[i] = list(sys.stdin.readline().strip())
+
+cnt = 0
+visited = [[False]*n for _ in range(n)]
+def bfs(a,b,eyes,color):
+    dx = [0,0,-1,1]
+    dy = [-1,1,0,0]
+
     for i in range(4):
-        nx = x+dx[i]
-        ny = y+dy[i]
+        x = a + dx[i]
+        y = b + dy[i]
+        if x < 0 or x >= n or y < 0 or y >=n or visited[x][y]:
+            continue
 
-        if 0 <= nx < n and 0 <= ny < n and not visited[nx][ny]:
-            # 적록색약일 경우
-            if isblind == 'blind':
-                if color != 'B' and arr[nx][ny] != 'B':
-                    visited[nx][ny] = True
-                    dfs(nx, ny, color, isblind)
-                elif color == 'B' and arr[nx][ny] == 'B':
-                    visited[nx][ny] = True
-                    dfs(nx, ny, color, isblind)
-                else:
-                    continue
-
-            # 적록색약 아닐 경우
+        if(eyes == 'color-blindness'):
+            if((color != 'B') and array[x][y] != 'B'):
+                visited[x][y] = True
+                bfs(x, y, eyes, color)
+            elif(color == 'B' and array[x][y] =='B'):
+                visited[x][y] = True
+                bfs(x, y, eyes, color)
             else:
-                if color == arr[nx][ny]:
-                    visited[nx][ny] = True
-                    dfs(nx, ny, color, isblind)
+                continue
+
+
+        else:
+            if color == array[x][y]:
+                visited[x][y] = True
+                bfs(x,y,eyes,color)
+
     return
 
-
-dx = [0, 0, 1, -1]
-dy = [1, -1, 0, 0]
-
-n = int(input())
-visited = [[False] * n for _ in range(n)]
-arr = [list(input().rstrip()) for _ in range(n)]
-
-count = 0
-blind_count = 0
-
-# 적록색약 아닐 경우
 for i in range(n):
     for j in range(n):
         if not visited[i][j]:
-            dfs(i, j, arr[i][j], '!blind')
-            count += 1
+            bfs(i,j,'!color-blindness',array[i][j])
+            cnt += 1
 
-# 적록 색약일 경우
-# visitied 배열 초기화
-visited = [[False] * n for _ in range(n)]
+print(cnt,end=' ')
+visited= [[False]*n for _ in range(n)]
+cnt = 0
 for i in range(n):
     for j in range(n):
-        if not visited[i][j]:
-            dfs(i, j, arr[i][j], 'blind')
-            blind_count +=1
-
-print(count, blind_count)
+        if not visited[i][j] :
+            bfs(i,j,'color-blindness',array[i][j])
+            cnt += 1
+print(cnt)
