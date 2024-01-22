3402387741242a75322b8ac5ecfb966a1e3e4fee
writtenbyrla <133620285+writtenbyrla@users.noreply.github.com>
2024-01-22 10:29:38 +0900

    [Silver III] Title: 퇴사, Time: 48 ms, Memory: 31252 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.py
+++ 백준/Silver/14501. 퇴사/퇴사.py
@@ -5,6 +5,10 @@ N = int(input())
 table = [tuple(map(int, input().split())) for _ in range(N)]
 
 max_val = 0
+
+# dp(n번째 날, 여태까지 번 돈)
+# -> 근무를 한다면 dp(n+t, 여태까지 번 돈 + n번째날에 번 돈)
+# -> 근무를 안한다면 dp(n+1, 여태까지 번 돈)
 def dp(idx, pay):
     global max_val
     if idx >= N:
@@ -15,7 +19,7 @@ def dp(idx, pay):
     t, p = table[idx]
 
     for i in range(2):
-        if i==1:
+        if i==1:  # 근무하는 경우
             if idx+t > N:
                 return
             dp(idx+t, pay+p)
