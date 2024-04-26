40a2f799e18ae2189892f2f0871e11b500a5c905
writtenbyrla <133620285+writtenbyrla@users.noreply.github.com>
2024-04-26 16:05:33 +0900

    [level 0] Title: 홀수 vs 짝수, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/181887. 홀수 vs 짝수/홀수 vs 짝수.py
+++ 프로그래머스/0/181887. 홀수 vs 짝수/홀수 vs 짝수.py
@@ -1,5 +1,7 @@
 def solution(num_list):
-    if sum(num_list[0::2]) < sum(num_list[1::2]):
-        return sum(num_list[1::2])
-    else:
-        return sum(num_list[0::2])
+    # if sum(num_list[0::2]) < sum(num_list[1::2]):
+    #     return sum(num_list[1::2])
+    # else:
+    #     return sum(num_list[0::2])
+    
+    return max(sum(num_list[0::2]), sum(num_list[1::2]))
