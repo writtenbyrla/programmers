752f493c09d4429367ba70b9bd66fbde880d47a3
writtenbyrla <133620285+writtenbyrla@users.noreply.github.com>
2024-05-09 08:28:41 +0900

    [level 0] Title: 자릿수 더하기, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.py
+++ 프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.py
@@ -1,6 +1,8 @@
 def solution(n):
-    number = str(n)
-    answer = 0
-    for num in number:
-        answer += int(num)
-    return answer
+    # number = str(n)
+    # answer = 0
+    # for num in number:
+    #     answer += int(num)
+    # return answer
+    
+    return sum(int(i) for i in str(n))
