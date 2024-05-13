381adc88d8b0c32a59a8f2ba7b74c86f15a16b2a
writtenbyrla <133620285+writtenbyrla@users.noreply.github.com>
2024-05-13 10:48:31 +0900

    [level 0] Title: 등차수열의 특정한 항만 더하기, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181931. 등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기.py
@@ -0,0 +1,11 @@
+def solution(a, d, included):
+    answer = 0
+    arr=[]
+    for i in range(len(included)):
+        arr.append(a+(d*i))
+    
+    for i in range(len(included)):
+        if included[i]:
+            answer += arr[i]
+        
+    return answer
